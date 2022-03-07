90e9c9a5cd623a798841aa6ec5e9b37a5848015a
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-07 16:28:16 +0100

    adding card by javascript

---
+++ script.js
@@ -0,0 +1,46 @@
+const resultTeam = document.getElementById('team');
+let container = document.querySelector('.team-container');
+const namePeople = [
+    {
+        name: 'Wayne barnett',
+        job: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        job: 'Chif Editor',
+        image: 'angela-carol-chif-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        job: 'Social Media Menager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        job: 'Dveloper',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        job: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    },
+
+];
+
+for (let i = 0; i < 6; i++) {
+    let cardUser = document.createElement('team-card');
+    cardUser.classList.add('card-image');
+    cardUser.classList.add('card-text');
+    cardUser.innerHTML = namePeople[i].name;
+    cardUser.innerHTML = namePeople[i].job;
+
+    container.append(cardUser);
+}
+
