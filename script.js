e4d37ca013df6c195925d48cfd39338190ec843f
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-07 18:04:46 +0100

    finish

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 const resultTeam = document.getElementById('team');
-let container = document.querySelector('.team-container');
+const container = document.querySelector('.team-container');
+let nameUser = document.getElementById('name');
+let roleUser = document.getElementById('role');
+const btnAdd = document.getElementById('addMemberButton');
+let image = document.getElementById('image');
 
 const namePeople = [
     {
         name: 'Wayne barnett',
         job: 'Founder & CEO',
-        image: 'img/wayne-barnett-founder-ceo.jpg'
+        image: 'img/wayne-barnett-founder-ceo.jpg',
     },
     {
         name: 'Angela Caroll',
         job: 'Chif Editor',
-        image: 'img/angela-caroll-chief-editor.jpg'
+        image: 'img/angela-caroll-chief-editor.jpg',
     },
     {
         name: 'Walter Gordon',
         job: 'Office Manager',
-        image: 'img/walter-gordon-office-manager.jpg'
+        image: 'img/walter-gordon-office-manager.jpg',
     },
     {
         name: 'Angela Lopez',
         job: 'Social Media Menager',
-        image: 'img/angela-lopez-social-media-manager.jpg'
+        image: 'img/angela-lopez-social-media-manager.jpg',
     },
     {
         name: 'Scott Estrada',
@@ -35,14 +39,35 @@ const namePeople = [
 
 ];
 
+//add all card
 
-let image = document.createElement('img');
-for (let i = 0; i < 6; i++) {
-    let cardUser = document.createElement('team-card');
-    cardUser.classList.add('card-image');
-    cardUser.classList.add('card-text');
-    cardUser.innerHTML = `<img src="${namePeople[i].image}"> <h3>${namePeople[i].name}</h3> <p>${namePeople[i].job}</p>`;
+for (let i = 0; i < namePeople.length; i++) {
+    generateCard(namePeople[i].image, namePeople[i].name, namePeople[i].job)
+}
+
+
+function generateCard (image, names, role) {
+    let cardUser = document.createElement('div');
+    cardUser.classList.add('team-card');
+    cardUser.innerHTML = `
+    <div class="card-image"><img src="${image}"/></div> 
+    <div class="card-text">
+    <h3>${names}</h3> 
+    <p>${role}</p>
+    </div>`;
 
     container.append(cardUser);
 }
+
+// add card with input
+btnAdd.addEventListener('click', function() {
+    let newUser = {
+        name: nameUser.value,
+        job: roleUser.value,
+        image: image.value
+    }
+
+    namePeople.push(newUser);
+    generateCard(image.value, nameUser.value, roleUser.value);
+})
 
